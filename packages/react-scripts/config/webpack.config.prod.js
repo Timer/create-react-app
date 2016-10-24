79ceab3318fabc7e7612d7222a062f3c270c5433
Joe Haddad <timer150@gmail.com>
2016-10-24 01:36:25 -0400

    Add BuildProgressPlugin

--- packages/react-scripts/config/webpack.config.prod.js
+++ packages/react-scripts/config/webpack.config.prod.js
@@ -19,6 +19,7 @@ var SubresourceIntegrityPlugin = require('webpack-subresource-integrity');
 var url = require('url');
 var paths = require('./paths');
 var getClientEnvironment = require('./env');
+var BuildProgressPlugin = require('react-dev-utils/BuildProgressPlugin');
 
 // @remove-on-eject-begin
 // `path` is not used after eject - see https://github.com/facebookincubator/create-react-app/issues/1174
@@ -81,7 +82,7 @@ module.exports = {
     // We don't currently advertise code splitting but Webpack supports it.
     filename: 'static/js/[name].[chunkhash:8].js',
     chunkFilename: 'static/js/[name].[chunkhash:8].chunk.js',
-    // Given Webpack supports codesplit and production bundles are using 
+    // Given Webpack supports codesplit and production bundles are using
     // subresource integrity, it's important to make sure the attribute
     // set on async-loaded chunks is set to anonymous.
     crossOriginLoading: 'anonymous',
@@ -277,7 +278,8 @@ module.exports = {
     // Generate and inject subresources hashes in the final `index.html`.
     new SubresourceIntegrityPlugin({
       hashFuncNames: ['sha256', 'sha384']
-    })
+    }),
+    new BuildProgressPlugin()
   ],
   // Some libraries import Node modules but don't use them in the browser.
   // Tell Webpack to provide empty mocks for them so importing them works.
